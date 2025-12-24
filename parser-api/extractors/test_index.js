e0d0521db6af038548eff717d180b8c68de06c14
Liago <liago@dev.me>
2025-12-24 17:54:16 +0100

    chore: code cleaning

--- parser-api/extractors/test_index.js
+++
@@ -1,8 +0,0 @@
-const { extractors } = require('./index.js');
-
-console.log('Registered domains:');
-extractors.forEach(e => {
-    if (e.domain.includes('ultimouomo')) {
-        console.log(e.domain);
-    }
-});
